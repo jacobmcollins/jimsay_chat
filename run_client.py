0b71f872509fedf090784a2c69b7934879869ad0
jacob <jacob.m.collins2@gmail.com>
2018-11-17 13:46:08 -0800

    Added GUI for client - needs cleanup

--- run_client.py
+++ run_client.py
@@ -1,5 +1,9 @@
 from client.pi3.JPCClient import JPCClient
+import argparse
 
 if __name__ == '__main__':
-    client = JPCClient('192.168.0.21')
+    parser = argparse.ArgumentParser()
+    parser.add_argument("ip", type=str, help="server ip address")
+    args = parser.parse_args()
+    client = JPCClient(args.ip)
     client.run()
