3a62fe567413192bfc11ccdb7061e7d9f516a6d1
jacob <jacob.m.collins2@gmail.com>
2018-11-18 18:20:28 -0800

    Added rainier.jpg to client GUI

--- client/pi3/JPCClientGUI.py
+++ client/pi3/JPCClientGUI.py
@@ -1,5 +1,6 @@
 import time
 from tkinter import *
+from PIL import ImageTk,Image
 
 
 class JPCClientGUI:
@@ -10,6 +11,14 @@ class JPCClientGUI:
         self.message_text.set("Welcome")
         self.label = Label(self.root, textvariable=self.message_text,font=("Helvetica", 50), wraplength=500, justify=LEFT)
         self.label.pack(expand=True)
+        path = "server\gui\Rainier.jpg"
+        import os
+        x = os.path.join(os.getcwd(),path)
+        print(x)
+        self.img = ImageTk.PhotoImage(Image.open(x))
+        self.panel = Label(self.root, image=self.img)
+        self.panel.pack()
+        self.panel.update()
 
     def start(self):
         self.root.update()
